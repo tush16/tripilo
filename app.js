bb0fde40d9dd17429b468f58d6f02cfa2d09371f
Tushar Choudhary <tusharchoudhary8051@gmail.com>
2022-07-25 00:37:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+//1) MIDDLEWARES
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//3) ROUTES
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+//4) START SERVER
+
+module.exports = app;
